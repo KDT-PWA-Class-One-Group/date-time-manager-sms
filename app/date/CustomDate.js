7f76ae4ea1e92cc5b8242ef6ebbe9dac665f2f5b
Pukkok <krystalgggg@gmail.com>
2024-12-13 16:17:59 +0900

    string format 형태로 넣은 경우 테스트 완료

--- app/date/CustomDate.js
+++ app/date/CustomDate.js
@@ -5,9 +5,16 @@ class CustomDate {
   }
 
   set maker (value) {
+    // ? 아무 값도 안넣은 경우
     if(value.length === 0) {
       console.log('1번 케이스')
       return this._date = new Date()
+    }
+    // ? string format 형태로 넣은 경우
+    if(value.length === 1 && typeof value[0] === "string") {
+      console.log('2번에 들어옴')
+      const stringFilter = value[0].replace(/[!@#$%^&*(),\.\/]/g, "-")
+      return this._date = new Date(stringFilter)
     }
   }
 
@@ -16,6 +23,8 @@ class CustomDate {
   }
   
 }
+const date = new Date("2023-12-11")
+console.log(date.toISOString())
 
-const cd = new CustomDate()
+const cd = new CustomDate("2023-12-11")
 console.log(cd.maker)
